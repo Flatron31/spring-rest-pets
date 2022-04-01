e33e0ca1e919f3829ee3727379f469509b794ad3
Alexandr <akvadivchik@gmail.com>
2022-04-01 21:09:54 +0300

    Initial commit

---
+++ src/main/java/com/example/springrestpets/SpringRestPetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrestpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestPetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestPetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrestpets/SpringRestPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestPetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
